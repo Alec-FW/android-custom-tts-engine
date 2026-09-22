103bf39065c810e6be980b8b4649237502ff38ed
Alec-FW <391491+Alec-FW@users.noreply.github.com>
2026-09-22 17:44:29 +0300

    fix deprecation warning

--- app/src/main/java/com/example/CustomTts/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/CustomTts/ui/SettingsScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -112,7 +112,7 @@ fun SettingsScreen(
                 title = { Text(stringResource(id = R.string.settings_title)) }, // Changed
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = stringResource(id = R.string.settings_back_description)) // Changed
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = stringResource(id = R.string.settings_back_description)) // Changed
                     }
                 }
             )
